7fb9d310bfbb624997061c69b5d1f0e44f6b2f33
sharafkar <amirh.sharafkar@gmail.com>
2019-02-24 11:20:38 +0330

    implement channel and chat view controller

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,24 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Amir on 2/23/19.
+//  Copyright © 2019 Amir Sharafkar. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+
+}
