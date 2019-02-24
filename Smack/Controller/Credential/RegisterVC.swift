7df4aaffd5644eb85d9094c54e3e8f89ae49242a
sharafkar <amirh.sharafkar@gmail.com>
2019-02-24 18:53:52 +0330

    implement credential

---
+++ Smack/Controller/Credential/RegisterVC.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterVC.swift
+//  Smack
+//
+//  Created by Amir on 2/24/19.
+//  Copyright © 2019 Amir Sharafkar. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Amir on 2/24/19.
+//  Copyright © 2019 Amir Sharafkar. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
+let TO_CREATE_ACCOUNT = "toCreateAccount"
