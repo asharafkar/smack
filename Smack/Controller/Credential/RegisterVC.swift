fd8578835a2cfb80ec56f24f60a43165b3cc34e8
sharafkar <amirh.sharafkar@gmail.com>
2019-02-26 15:02:28 +0330

    implement auth service

--- Smack/Controller/Credential/RegisterVC.swift
+++ Smack/Controller/Credential/RegisterVC.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class RegisterVC: UIViewController {
 
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var userAvatarImage: UIImageView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -22,4 +28,28 @@ class RegisterVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func pickAvatarPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func pickBGColorPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func createAccountPressed(_ sender: UIButton) {
+        guard let email = emailText.text, emailText.text != "" else {
+            return
+        }
+        
+        guard let password = passwordText.text, passwordText.text != "" else {
+            return
+        }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("user has been registered!")
+            }
+        }
+    }
 }
